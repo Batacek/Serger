fdfa10c2a44535903fb4d783f217e9d75835d198
Thomas <74470527+Batacek@users.noreply.github.com>
2024-11-10 15:50:45 +0100

    Adding languages
    Languages can be now changed. Currently avaibled: EN, CS

--- Serger/Program.cs
+++ Serger/Program.cs
@@ -111,7 +111,7 @@ public class Config                                     // Class for configurati
     public string JSON_VER { get; set; }                // JSON config file version (X.y.X)
     public int PING_DELAY { get; set; }                 // Pause lenght between pings (in ms)
     public string LANG { get; set; }                    // Language (currently: en/cz)
-    public string CS_Version = "0.0.4";                 // CS Program file version (X.X.y)
+    public string CS_Version = "0.0.5";                 // CS Program file version (X.X.y)
     public string CS_VER = "0.0";                       // CS Program file version (X.y.X)
 }
 
