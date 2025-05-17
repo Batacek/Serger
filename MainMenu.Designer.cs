8735b93d65b4c41dbee235881f1511c32671557a
Thomas <74470527+Batacek@users.noreply.github.com>
2025-05-17 19:01:46 +0200

    Add files via upload

---
+++ MainMenu.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Serger
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.settingsPanel = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lang = new System.Windows.Forms.TextBox();
+            this.jsonVersion2TextBox = new System.Windows.Forms.TextBox();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.pingDelay = new System.Windows.Forms.TextBox();
+            this.jsonVersionTextBox = new System.Windows.Forms.TextBox();
+            this.pingerPanel = new System.Windows.Forms.Panel();
+            this.pingResult = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ping = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.settingsPanel.SuspendLayout();
+            this.pingerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.ForeColor = System.Drawing.Color.White;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(151, 450);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(144, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(37, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 24);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "MENU";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(3, 57);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(148, 35);
+            this.panel3.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 29);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Pinger";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(3, 98);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(148, 35);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Settings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // settingsPanel
+            // 
+            this.settingsPanel.Controls.Add(this.label6);
+            this.settingsPanel.Controls.Add(this.button3);
+            this.settingsPanel.Controls.Add(this.label5);
+            this.settingsPanel.Controls.Add(this.label4);
+            this.settingsPanel.Controls.Add(this.label3);
+            this.settingsPanel.Controls.Add(this.label2);
+            this.settingsPanel.Controls.Add(this.label1);
+            this.settingsPanel.Controls.Add(this.lang);
+            this.settingsPanel.Controls.Add(this.jsonVersion2TextBox);
+            this.settingsPanel.Controls.Add(this.urlTextBox);
+            this.settingsPanel.Controls.Add(this.pingDelay);
+            this.settingsPanel.Controls.Add(this.jsonVersionTextBox);
+            this.settingsPanel.Location = new System.Drawing.Point(157, 12);
+            this.settingsPanel.Name = "settingsPanel";
+            this.settingsPanel.Size = new System.Drawing.Size(517, 426);
+            this.settingsPanel.TabIndex = 1;
+            this.settingsPanel.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(23, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Settings";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Submit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Language";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ping Delay";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "JSON Version 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "URL/IP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "JSON Version 1";
+            // 
+            // lang
+            // 
+            this.lang.Location = new System.Drawing.Point(250, 168);
+            this.lang.Name = "lang";
+            this.lang.Size = new System.Drawing.Size(100, 20);
+            this.lang.TabIndex = 4;
+            // 
+            // jsonVersion2TextBox
+            // 
+            this.jsonVersion2TextBox.Location = new System.Drawing.Point(250, 86);
+            this.jsonVersion2TextBox.Name = "jsonVersion2TextBox";
+            this.jsonVersion2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.jsonVersion2TextBox.TabIndex = 3;
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(250, 115);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(100, 20);
+            this.urlTextBox.TabIndex = 2;
+            // 
+            // pingDelay
+            // 
+            this.pingDelay.Location = new System.Drawing.Point(250, 142);
+            this.pingDelay.Name = "pingDelay";
+            this.pingDelay.Size = new System.Drawing.Size(100, 20);
+            this.pingDelay.TabIndex = 1;
+            // 
+            // jsonVersionTextBox
+            // 
+            this.jsonVersionTextBox.Location = new System.Drawing.Point(250, 60);
+            this.jsonVersionTextBox.Name = "jsonVersionTextBox";
+            this.jsonVersionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.jsonVersionTextBox.TabIndex = 0;
+            // 
+            // pingerPanel
+            // 
+            this.pingerPanel.Controls.Add(this.pingResult);
+            this.pingerPanel.Controls.Add(this.label7);
+            this.pingerPanel.Location = new System.Drawing.Point(157, 12);
+            this.pingerPanel.Name = "pingerPanel";
+            this.pingerPanel.Size = new System.Drawing.Size(517, 426);
+            this.pingerPanel.TabIndex = 2;
+            // 
+            // pingResult
+            // 
+            this.pingResult.AutoSize = true;
+            this.pingResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pingResult.Location = new System.Drawing.Point(30, 66);
+            this.pingResult.Name = "pingResult";
+            this.pingResult.Size = new System.Drawing.Size(51, 20);
+            this.pingResult.TabIndex = 13;
+            this.pingResult.Text = "label8";
+            this.pingResult.Click += new System.EventHandler(this.pingResult_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(29, 11);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Pinger";
+            // 
+            // ping
+            // 
+            this.ping.Tick += new System.EventHandler(this.ping_Tick);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 450);
+            this.Controls.Add(this.pingerPanel);
+            this.Controls.Add(this.settingsPanel);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.settingsPanel.ResumeLayout(false);
+            this.settingsPanel.PerformLayout();
+            this.pingerPanel.ResumeLayout(false);
+            this.pingerPanel.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel settingsPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lang;
+        private System.Windows.Forms.TextBox jsonVersion2TextBox;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.TextBox pingDelay;
+        private System.Windows.Forms.TextBox jsonVersionTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel pingerPanel;
+        private System.Windows.Forms.Label pingResult;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer ping;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
