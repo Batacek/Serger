1844ca12079a051660a57045981ecca3cea6bdaa
Batacek <74470527+Batacek@users.noreply.github.com>
2025-05-23 07:05:08 +0200

    Add Pinger class for handling network pings

---
+++ SRG_Core/Pinger.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.NetworkInformation;
+
+namespace SRG_Core;
+
+public class Pinger : Main
+{
+    private readonly Config _config;
+    //private readonly LangDictionary _langDictionary;
+
+    public Pinger(Config config)
+    {
+        this._config = config;
+        //_langDictionary = langDictionary;
+    }
+
+    public async Task PingAddr()
+    {
+        try
+        {
+            PingReply reply = new Ping().Send(_config.URL); // Ping
+            Console.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffff")}"); // Date and time
+            if (reply.Status == IPStatus.Success) // if condition for success case
+            {
+                // Printing the info into console window and log file
+                Log.PrintLog($"Ping {_config.URL} Success.");
+                Log.PrintLog($"Address: {reply.Address}");
+                Log.PrintLog($"Ping time: {reply.RoundtripTime} ms");
+                Log.PrintLog($"TTL: {reply.Options.Ttl}\n");
+            }
+            else
+            {
+                Log.PrintLog($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}");
+                Log.PrintLog($"Ping {_config.URL} Fail: {reply.Status}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(
+                $"Ping {_config.URL} Fail. \nError: {ex.Message}"); // Printout the error message
+            Console.WriteLine();
+        }
+
+        await Task.Delay(_config.PING_DELAY); // Pause between pings
+    }
+
+}
