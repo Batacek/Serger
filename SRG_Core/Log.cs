ee3d968a73ba2dc02f45ce8ebc6962239c512a8c
Batacek <74470527+Batacek@users.noreply.github.com>
2025-06-05 14:14:10 +0200

    Add inline comments to Log class methods to clarify usage
    Signed-off-by: Batacek <74470527+Batacek@users.noreply.github.com>

--- SRG_Core/Log.cs
+++ SRG_Core/Log.cs
@@ -5,16 +5,16 @@ public class Log : Main
     private static readonly string LogFile = $"{DateTime.Now:dd_MM_yyyy}.log";
     private static readonly string LogAllFile = $"{DateTime.Now:dd_MM_yyyy}_ALL.log";
 
-    public static void WriteLog(string message)
-    {
+    public static void WriteLog(string message)     // Provides logging functionality for writing messages to daily log files without console output
+    {                                               // Use this method for error messages and important information only
         using (StreamWriter logging = new StreamWriter(LogFile, true))
         {
             logging.WriteLine(message);
         }
     }
 
-    public static void PrintLog(string message)
-    {
+    public static void PrintLog(string message)     // Provides logging functionality for writing messages to daily log files and console output
+    {                                               // Use this method for messages for everything that isn't using WriteLog that you want to see in the console as well
         using (StreamWriter logging = new StreamWriter(LogAllFile, true))
         {
             logging.WriteLine(message);
@@ -22,8 +22,8 @@ public class Log : Main
         }
     }
 
-    public static void LogAll(string mesage)
-    {
+    public static void LogAll(string mesage)        // Provides logging functionality for writing messages to daily log files and console output
+    {                                               // Use this method for messages for everything that isn't using WriteLog that you don't want to see in the console
         using (StreamWriter logging = new StreamWriter(LogAllFile, true))
         {
             logging.WriteLine(mesage);
