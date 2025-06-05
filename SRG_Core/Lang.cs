fbb0123919eb4f67851171539d31e73a7a03d3d8
Batacek <74470527+Batacek@users.noreply.github.com>
2025-06-05 14:16:00 +0200

    Add Lang class to SRG_Core with placeholder for future language support
    Signed-off-by: Batacek <74470527+Batacek@users.noreply.github.com>

---
+++ SRG_Core/Lang.cs
@@ -0,0 +1,8 @@
+﻿namespace SRG_Core;
+
+public class Lang
+{
+    // TODO: Implement language support
+    // GUI will use ouput of other classes which will use this class to get the language strings
+    // Use the defualt (english) output until the language support is implemented
+}
